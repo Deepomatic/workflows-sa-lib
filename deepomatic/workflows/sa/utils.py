d68eeef128844dd3d7c5fc0f83e616897884cc0f
antbertrand <antoine.bertrand@deepomatic.com>
2023-04-28 10:28:10 +0200

    add parameter to define image entry name

--- deepomatic/workflows/sa/utils.py
+++ deepomatic/workflows/sa/utils.py
@@ -3,12 +3,13 @@ import copy
 from deepomatic.workflows.v2.core import Region, Bbox
 
 
-def create_region_from_prediction(preds, crop_reference_bbox=None):
+def create_region_from_prediction(image_entry_name, preds, crop_reference_bbox=None):
     '''
     Will create a region in the format that Studio can plot
     Can also change the reference bbox used in all the predictions coordinates (by default it is the full image).
     (Useful for detection in detection, where you want the coordinates to be those of the full image)
     Args:
+        image_entry_name (str): Name of the image entry that the predictions are related to.
         preds (list or dict): List of predictions or single prediction, in the processed dict format.
         crop_reference_bbox (dict): Bbox of the crop that is the actual reference of the bbox in preds.
     '''
@@ -33,7 +34,7 @@ def create_region_from_prediction(preds, crop_reference_bbox=None):
             pred['bbox'] = bbox
 
         region = Region(
-            entry_name='image',
+            entry_name=image_entry_name,
             bbox=Bbox(xmin=pred['bbox']['xmin'], ymin=pred['bbox']['ymin'],
                       xmax=pred['bbox']['xmax'], ymax=pred['bbox']['ymax']),
             label_name=pred.get('concept', 'no_concept'),
