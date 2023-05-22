1f7cc3a7011c0882eb929ab6ada28089f6014b47
antbertrand <antoine.bertrand@deepomatic.com>
2023-05-22 17:36:32 +0200

    fix logging config

--- deepomatic/workflows/sa/logging_config.py
+++ deepomatic/workflows/sa/logging_config.py
@@ -1,11 +1,14 @@
 import logging
 
 logger = logging.getLogger(__name__)
+logger.propagate = False
+logger.setLevel(logging.INFO)
+
 # To switch if process/thread info nessary
 FORMAT = "[%(levelname)s %(name)s %(filename)s:%(lineno)s] %(message)s"
 formatter = logging.Formatter(fmt=FORMAT)
-handler = logging.StreamHandler()
-handler.setFormatter(formatter)
-logger.addHandler(handler)
-logger.setLevel(logging.INFO)
-logger.propagate = False
+
+if not logger.hasHandlers():
+    handler = logging.StreamHandler()
+    handler.setFormatter(formatter)
+    logger.addHandler(handler)
